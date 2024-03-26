324dff7d62aa0b72d841712b0672637486bcece1
haxx797 <65160092@go.buu.ac.th>
2024-03-26 11:11:48 +0700

    css

--- LaravelCRUD/resources/views/companies/create.blade.php
+++ LaravelCRUD/resources/views/companies/create.blade.php
@@ -1,10 +1,11 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>create data</title>
+    <title>Create Data</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+</head>
 <body>
     
     <div class="container mt-2">
@@ -12,47 +13,32 @@
             <div class="col-lg-12">
                 <h2>Add Company</h2>
             </div>
-            <div>
-                <a href="{{ route('companies.index')}}" class="btn btn-primary"> Back</a>
-            </div>
-            @if (session('status'))
-                <div class="alert alert-success">
-                    {{ session('status')}}
-                </div>
-            @endif
-            <form action="{{ route('companies.store')}}" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('companies.store')}}" method="POST" enctype="multipart/form-data" class="col-md-12">
                 @csrf
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group my-1">
-                            <strong>Company Name</strong>
-                            <input type="text" name="name" class="form-control" placeholder="Company Name">
-                            @error('name')
-                                <div class="alert alert-danger">{{ $message}}</div>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <div class="form-group my-1">
-                            <strong>Company Email</strong>
-                            <input type="email" name="email" class="form-control" placeholder="Company email">
-                            @error('email')
-                                <div class="alert alert-danger">{{ $message}}</div>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <div class="form-group my-1">
-                            <strong>Company Address</strong>
-                            <input type="text" name="address" class="form-control" placeholder="Company address">
-                            @error('address')
-                                <div class="alert alert-danger">{{ $message}}</div>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="cal-md-12">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
+                <div class="form-group my-1">
+                    <strong>Company Name</strong>
+                    <input type="text" name="name" class="form-control" placeholder="Company Name">
+                    @error('name')
+                        <div class="alert alert-danger">{{ $message}}</div>
+                    @enderror
+                </div>
+                <div class="form-group my-1">
+                    <strong>Company Email</strong>
+                    <input type="email" name="email" class="form-control" placeholder="Company email">
+                    @error('email')
+                        <div class="alert alert-danger">{{ $message}}</div>
+                    @enderror
+                </div>
+                <div class="form-group my-1">
+                    <strong>Company Address</strong>
+                    <input type="text" name="address" class="form-control" placeholder="Company address">
+                    @error('address')
+                        <div class="alert alert-danger">{{ $message}}</div>
+                    @enderror
+                </div>
+                <div class="form-group mt-2">
+                    <button type="submit" class="btn btn-primary float-end">Submit</button>
+                    <a href="{{ route('companies.index')}}" class="btn btn-secondary float-start">Back</a>
                 </div>
             </form>
         </div>
